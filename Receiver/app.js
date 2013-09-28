48e413b24fde56ed6c7ef33551215321c3143f7f
Vic Cekvenich <puppetMaster3 on twitter>
2013-09-28 16:02:11 -0700

    in

--- Receiver/app.js
+++ Receiver/app.js
@@ -1,2 +1,22 @@
-console.log('loaded');
+console.log('loaded1');
+
+//document.body.style.cursor = 'wait'
+require([
+    'assets/jquery-2.1.b1.js',
+    'assets/TweenLite.min.js',
+    'assets/blueGrass.js',
+    'assets/CSSPlugin.min.js'
+], function () {
+    CSSPlugin.defaultTransformPerspective = 500;
+    forward('topic1', 'topic1', onTopic);
+});
+
+duration = 1;
+viewDir = 'view/';
+function onTopic(nID) {
+    console.log('topic1');
+    var topic1 = document.getElementById(nID);
+    console.log(topic1);
+    TweenLite.to(topic1, duration, { rotationX: -40, x: 100 });
+}
 //# sourceMappingURL=app.js.map
--- Receiver/app.ts
+++ Receiver/app.ts
@@ -1,1 +1,24 @@
-console.log('loaded')
+
+console.log('loaded1')
+
+//document.body.style.cursor = 'wait'
+require(['assets/jquery-2.1.b1.js'
+	,'assets/TweenLite.min.js'
+	,'assets/blueGrass.js'
+	,'assets/CSSPlugin.min.js'   // Tween animation plugin
+
+], function() { // we loaded
+	CSSPlugin.defaultTransformPerspective = 500;
+	forward('topic1','topic1',onTopic)
+})
+
+duration = 1
+viewDir = 'view/'
+function onTopic(nID) {
+	console.log('topic1')
+	var topic1 = document.getElementById(nID)
+	console.log(topic1)
+	TweenLite.to(topic1,duration, { rotationX:-40,x:100, })
+}
+
+

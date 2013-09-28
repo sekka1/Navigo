89869182001204b4f599694f92008e14baed116e
Vic Cekvenich <puppetMaster3 on twitter>
2013-09-28 16:56:45 -0700

    sock

--- Receiver/app.js
+++ Receiver/app.js
@@ -5,12 +5,24 @@ require([
     'assets/jquery-2.1.b1.js',
     'assets/TweenLite.min.js',
     'assets/blueGrass.js',
-    'assets/CSSPlugin.min.js'
+    'assets/CSSPlugin.min.js',
+    'http://ws.algorithms.io/socket.io/socket.io.js'
 ], function () {
     CSSPlugin.defaultTransformPerspective = 500;
     forward('topic1', 'topic1', onTopic);
+
+    var socket = io.connect('http://ws.algorithms.io', function () {
+        socket.on('query_get_last_motion_data', onSocket);
+    });
 });
 
+function onSocket(data) {
+    console.log(data.rotation_x);
+    console.log(data.rotation_y);
+    console.log(data.rotation_x);
+}
+
+//http://www.greensock.com/css3
 duration = 1;
 viewDir = 'view/';
 function onTopic(nID) {

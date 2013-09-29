bd7e3d7dd6d210912bf8b0a1bc9f89403457cbb8
Vic Cekvenich <puppetMaster3 on twitter>
2013-09-29 15:07:03 -0700

    touch

--- Receiver/bind.js
+++ Receiver/bind.js
@@ -1,3 +1,11 @@
 function bindInit() {
 	console.log('initBind')
+	setTimeout(function(){
+		cAPI = new CloudAPI('fiasqrx5mli')
+		cAPI.disq('th','cnn',null,onTh)
+	},1000)
+}
+
+function onTh(data) {
+	console.log(JSON.stringify(data))
 }

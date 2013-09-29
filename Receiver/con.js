f05a1e4912f938d7018603600437fac57c2922c1
sekka1 <garlandk@gmail.com>
2013-09-29 12:24:01 -0700

    adding cnn data

--- Receiver/con.js
+++ Receiver/con.js
@@ -5,7 +5,7 @@ function conInit() {
 
 
 // sock *****************************
-//var socket = io.connect('http://ws.algorithms.io')
+var socket = io.connect('http://ws.algorithms.io')
 setTimeout(function () {
-	//socket.on('query_get_last_motion_data', onSocket)
+	socket.on('query_get_last_motion_data', onSocket)
 }, 500);

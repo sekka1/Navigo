452f631aa2c3e5b87f8979a0b28d5dd7c35d7aeb
Vic Cekvenich <puppetMaster3 on twitter>
2013-09-28 18:06:59 -0700

    starting on selected

--- Receiver/app.js
+++ Receiver/app.js
@@ -6,15 +6,32 @@ require([
     'assets/TweenLite.min.js',
     'assets/blueGrass.js',
     'assets/CSSPlugin.min.js',
-    'http://ws.algorithms.io/socket.io/socket.io.js'
+    'http://ws.algorithms.io/socket.io/socket.io.js',
+    'assets/more/keymaster.min.js'
 ], function () {
     CSSPlugin.defaultTransformPerspective = 500;
-    forward('card', 'card', onCard);
+    openCats();
 
     //var socket = io.connect('http://ws.algorithms.io')
     setTimeout(function () {
         //socket.on('query_get_last_motion_data', onSocket)
     }, 500);
+
+    key('d', function () {
+        console.log('right');
+    });
+    key('a', function () {
+        console.log('left');
+    });
+    key('w', function () {
+        console.log('up');
+    });
+    key('s', function () {
+        console.log('down');
+    });
+    key('q', function () {
+        console.log('home');
+    });
 });
 
 function onSocket(data) {
@@ -24,19 +41,26 @@ function onSocket(data) {
 }
 
 //http://www.greensock.com/css3
-var setDur = .5;
+var setDur = .3;
 viewDir = 'view/';
 var categories = [];
 var threads = [];
 var posts = [];
 var level = 1;
+var position = 1;
 function onCard(nID) {
     console.log(nID);
     categories.push(nID);
+    position++;
     var card = document.getElementById(nID);
-    TweenLite.to(card, setDur, { rotationX: -40, x: 100 });
+    TweenLite.from(card, setDur, { rotationX: -40 });
+    TweenLite.to(card, setDur, { scaleX: .8, scaleY: .8 });
 }
 
-function openTopics() {
+function openCats() {
+    var list = ["Hello", "World"];
+    for (var i = 0; i < list.length; i++) {
+        forward('cat', 'cat', onCard);
+    }
 }
 //# sourceMappingURL=app.js.map
--- Receiver/app.ts
+++ Receiver/app.ts
@@ -1,4 +1,3 @@
-
 console.log('loaded2')
 
 //document.body.style.cursor = 'wait'
@@ -7,14 +6,32 @@ require(['assets/jquery-2.1.b1.js'
 	,'assets/blueGrass.js'
 	,'assets/CSSPlugin.min.js'   // Tween animation plugin
 	,'http://ws.algorithms.io/socket.io/socket.io.js'
+	,'assets/more/keymaster.min.js'
 	], function() { // we loaded:
 		CSSPlugin.defaultTransformPerspective = 500;
-		forward('card','card',onCard)
+		openCats()
 
 		//var socket = io.connect('http://ws.algorithms.io')
 		setTimeout(function(){
 				//socket.on('query_get_last_motion_data', onSocket)
 			},500)
+
+
+		key('d',function(){
+			console.log('right')
+		})
+		key('a',function(){
+			console.log('left')
+		})
+		key('w',function(){
+			console.log('up')
+		})
+		key('s',function(){
+			console.log('down')
+		})
+		key('q',function(){
+			console.log('home')
+		})
 })
 
 function onSocket(data) {
@@ -24,19 +41,28 @@ function onSocket(data) {
 }
 
 //http://www.greensock.com/css3
-var setDur = .5
+var setDur = .3
 viewDir = 'view/'
 var categories  = [];
 var threads = [];
 var posts = [];
 var level:number=1;
+var position:number=1;
 function onCard(nID) {
 	console.log(nID)
 	categories.push(nID)
+	position++
 	var card = document.getElementById(nID)
-	TweenLite.to(card,setDur, { rotationX:-40,x:100, })
-}
-
-function openTopics() {
+	TweenLite.from(card,setDur, { rotationX:-40 })
+	TweenLite.to(card,setDur, { scaleX:.8, scaleY:.8 })
 
 }
+
+function openCats() {
+	var list = ["Hello","World"];
+	for (var i = 0; i < list.length; i++) {
+		forward('cat','cat',onCard)
+	}
+
+}
+
